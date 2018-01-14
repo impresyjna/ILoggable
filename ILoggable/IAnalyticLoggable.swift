f331e445ffc910d087e6b56627bf95dbe324dd6e
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2018-01-14 02:24:17 +0100

    Ready IAnalyticLoggable

--- ILoggable/IAnalyticLoggable.swift
+++ ILoggable/IAnalyticLoggable.swift
@@ -27,6 +27,11 @@ protocol IAnalyticLoggable: class {
     func appendEvent(type: AnalyticEventType, params: [String: Any]?)
 }
 
+enum WhichNavController: Int {
+    case current = 1
+    case previous = 2
+}
+
 extension IAnalyticLoggable where Self: UIViewController {
     func appendEvent(type: AnalyticEventType, params: [String: Any]? = nil) {
         switch type {
@@ -40,31 +45,51 @@ extension IAnalyticLoggable where Self: UIViewController {
     func appendViewVisited(params: [String: Any]? = nil) {
         var parentId: Int = 0
         if let navController = navigationController {
-            if let pController = getPreviousViewController(navController: navController) {
+            if let pController = getViewControllerFromNav(navController: navController) {
                 parentId = pController.analyticView.id
             }
         } else if let presentingViewContoller = self.presentingViewController as? UINavigationController {
-            if let pController = getPreviousViewController(navController: presentingViewContoller) {
+            if let pController = getViewControllerFromNav(navController: presentingViewContoller, which: .current) {
                 parentId = pController.analyticView.id
             }
+        } else if let presentViewContoller = self.presentingViewController, let pController = presentViewContoller as? IAnalyticLoggable {
+            parentId = pController.analyticView.id
         }
+        print(prepareParams(eventParams: [AnalyticEventNamespace.parentView: parentId, AnalyticEventNamespace.childView: analyticView.id], extraParams: params))
         print("View id \(analyticView.id), parent id: \(parentId)")
     }
     
     func appendButtonClicked(buttonId: Int, params: [String: Any]? = nil) {
-        print("Appending button clicked")
+        print("Button clicked View id \(buttonId), parent id: \(analyticView.id)")
     }
     
-    func getPreviousViewController(navController: UINavigationController?) -> IAnalyticLoggable? {
+    func getViewControllerFromNav(navController: UINavigationController?, which: WhichNavController = .previous) -> IAnalyticLoggable? {
         guard let navController = navController else {
             return nil
         }
         
         let count = navController.viewControllers.count
-        if count - 2 >= 0, let pController = navController.viewControllers[count - 2] as? IAnalyticLoggable {
+        if count - which.rawValue >= 0, let pController = navController.viewControllers[count - which.rawValue] as? IAnalyticLoggable {
             return pController
         }
         
         return nil
+    }
+    
+    func prepareParams(eventParams: [String: Any], extraParams: [String: Any]?) -> [String: Any] {
+        var params: [String: Any] = [:]
+        params[AnalyticEventNamespace.timestamp] = Date()
+        
+        for (key, value) in eventParams {
+            params[key] = value
+        }
+        
+        if let eParams = extraParams {
+            for (key, value) in eParams {
+                params[key] = value
+            }
+        }
+        
+        return params
     }
 }
